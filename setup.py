564a336cacc142980dfc1a5e02778b06c4abcc70
syedalimohsinbukhari <syedali.b@outlook.com>
2024-11-20 15:49:44 +0500

    [md-docs-minor-21]
    1. added `environment.yml`, updated `setup.py` and `pyproject.toml`
    2. added `version.py`
    3. minor modifications to `runge_kutta_multi.py`

--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
+"""Created on Dec 20 15:21:12 2023"""
+
+import os
+
 from setuptools import find_packages, setup
 
-with open('README.md', 'r') as f:
-    readme = f.read()
 
-# ext_modules = [
-#     Extension(
-#         'cpp_bindings',
-#         sources=['src/num_solvers/cpp_bindings/binder/bindings.cpp'],
-#         include_dirs=[pybind11.get_include(), "src/num_solvers/cpp_bindings/include"],
-#         language='c++',
-#         extra_compile_args=['-std=c++11'],
-#     ),
-# ]
+def load_metadata():
+    """get the metadata for the package."""
+    metadata = {}
+    with open(os.path.join("src", "num_solvers", "version.py")) as f:
+        exec(f.read(), metadata)
+    return metadata
 
-setup(name='num_solvers',
-      version='0.1.0',
-      packages=find_packages(where=["src", "src/num_solvers/cpp_bindings/binder"]),
-      url='https://github.com/syedalimohsinbukhari/Solvers',
-      license='MIT',
-      author='Syed Ali Mohsin Bukhari, Astrophysics and Python',
-      author_email='syedali.b@outlook.com, astrophysicsandpython@gmail.com',
-      description='A library for some basic numerical num_solvers.',
+
+metadata_ = load_metadata()
+
+with open('README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+setup(name='__name__',
+      version=metadata_['__version__'],
+      author=metadata_['__author__'],
+      author_email=metadata_['__email__'],
+      license=metadata_['__license__'],
+      url=metadata_['__url__'],
+      description=metadata_['__description__'],
+      packages=find_packages(where="src", exclude=["test"]),
+      package_dir={"": "src"},
       long_description=readme,
       long_description_content_type="text/markdown",
       python_requires=">=3.9",
       install_requires=["numpy==1.26.4", "setuptools", "custom-inherit", "umatrix"],
       include_package_data=True,
-      # ext_modules=ext_modules,
-      package_data={
-          'num_solvers': ['cpp_bindings/binder/*.cpp', 'cpp_bindings/include/*']
-      },
-      zip_safe=False,
       classifiers=[
           "License :: OSI Approved :: MIT License",
           "Programming Language :: Python :: 3.9",
